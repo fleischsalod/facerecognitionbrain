38f6b73487d7af453f3876c657802d8bdfc33501
Fleischsalod <graml.luis@web.de>
2018-09-24 17:44:44 +0200

    Upload new projectfiles for complete frontend

--- src/components/Logo/Logo.js
+++ src/components/Logo/Logo.js
@@ -9,6 +9,7 @@ const Logo = () => {
 			<Tilt className="Tilt br2 shadow-2" options={{ max : 55 }} style={{ height: 150, width: 150 }} >
 				<div className="Tilt-inner pa3">
 					<img style={{paddingTop: '5px'}} alt='logo' src={brain} />
+					<p>BRAIN</p>
 				</div>
 			</Tilt>
 		</div>
